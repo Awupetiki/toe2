4c65fbc907bfd4b424b5c94662ce3301446cbdae
medved404 <mihailnagr063@gmail.com>
2023-08-14 20:53:16 +0600

    Update MovingCamera.cs

--- Assets/MovingCamera.cs
+++ Assets/MovingCamera.cs
@@ -19,8 +19,5 @@ public class MovingCamera : MonoBehaviour
     void Update()
     {
         transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime);
-        // Clamp the zoom value between the min and max zoom levels
-        // newZoom = Mathf.Clamp(newZoom, minZoom, maxZoom);
-        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, cam.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, Time.deltaTime * zoomSpeed);
     }
 }
